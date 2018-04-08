4aca86c72a5cac1e28e3294feb9635e91b33fd49
lukasbs <lukaszbojes@gmail.com>
2018-04-08 16:31:28 +0200

    Added getting object by Name/Title

--- src/main/java/com/team/mybook/data/repository/BookRepository.java
+++ src/main/java/com/team/mybook/data/repository/BookRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface BookRepository extends CrudRepository<Book, Long> {
     Book findByTitle(String title);
+    void deleteBookByTitle(String title);
 }
